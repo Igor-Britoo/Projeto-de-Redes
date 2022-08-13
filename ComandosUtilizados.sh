9a4ba9e1cafa7d50b760bc9cfb4357a99b78cb3d
Igor212 <85748927+Igor212@users.noreply.github.com>
2022-08-13 15:34:36 -0300

    Definição de interfaces para as VLANs no terminal do EtherSwitch

--- ComandosUtilizados.sh
+++ ComandosUtilizados.sh
@@ -21,7 +21,7 @@
 	
 	
 	#Define interfaces para VLAN 35
-		interface range FastEthernet 1/8 - 15 #acessa o range de interfaces do 1/1 ao 1/7
+		interface range FastEthernet 1/8 - 15 #acessa o range de interfaces do 1/1 ao 1/15
 		switchport mode access #define a porta como uma porta de acesso para as VLANs
 		switchport access vlan 35 #define a porta como uma porta de acesso para VLAN 35
 		no shutdown #faz com que as interfaces nao desliguem
